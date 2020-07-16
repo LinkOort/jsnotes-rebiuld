bc99528146e6a5fed431d1f874aa39e4f719ba85
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2020-07-15 23:45:03 -0300

    updates

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function createNote() {
+    var divtoAdd = document.querySelector('.noteArea');
+    var noteTemplate = document.createElement("div");
+    noteTemplate.classList.add("sample");
+    divtoAdd.appendChild(noteTemplate);
+}
+function darkMode() {
+    
+}
