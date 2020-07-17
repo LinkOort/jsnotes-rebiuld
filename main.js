64891b77b3af7befe17c76cf0159be1c359cf867
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2020-07-16 22:19:38 -0300

    some updates

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-
+var bsave = document.querySelector('.save')
+bsave.addEventListener("click", function() {
+    createNote()
+    delButton()
+}) 
 function createNote() {
-    var divtoAdd = document.querySelector('.noteArea');
     var noteTemplate = document.createElement("div");
-    noteTemplate.classList.add("sample");
-    divtoAdd.appendChild(noteTemplate);
+    noteTemplate.setAttribute("id", "sample");
+    document.querySelector('.noteArea').appendChild(noteTemplate);
 }
-function darkMode() {
+function divtoappendButtons() {
+    var div = document.createElement("div");
+    div.setAttribute("id", "buttonarea");
+    document.getElementById("sample").appendChild(div);
+}
+function delButton() {
+    var btcreate = document.createElement("button");
+    btcreate.textContent = "Apagar"
+    btcreate.classList.add("btdel");
+    document.getElementById("buttonarea").appendChild(btcreate);
+}
+function editButton() {
+    var btcreate = document.createElement("button");
+    btcreate.textContent = "Editar"
+    btcreate.classList.add("btedit");
+    document.getElementById("buttonarea").appendChild(btcreate);
+}
+
+function appendText() {
     
+}
+
+function editNote() {
+
+}
+
+function deleteNote() {
+
+}
+
+function darkMode() {
+   
 }
