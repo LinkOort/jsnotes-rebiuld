4ca6872fdd7e677e8958c50a01519107f08053b9
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2020-07-17 02:21:29 -0300

    ref and updates

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-var bsave = document.querySelector('.save')
-bsave.addEventListener("click", function() {
-    createNote()
-    delButton()
-}) 
 function createNote() {
-    var noteTemplate = document.createElement("div");
-    noteTemplate.setAttribute("id", "sample");
-    document.querySelector('.noteArea').appendChild(noteTemplate);
+    var noteTemplate = document.createElement("div")
+    noteTemplate.setAttribute("id", "sample")
+    document.querySelector('.noteArea').appendChild(noteTemplate)
 }
+
 function divtoappendButtons() {
-    var div = document.createElement("div");
-    div.setAttribute("id", "buttonarea");
-    document.getElementById("sample").appendChild(div);
+    var div = document.createElement("div")
+    div.setAttribute("id", "buttonarea")
+    document.getElementById("sample").appendChild(div)
 }
+
 function delButton() {
-    var btcreate = document.createElement("button");
+    var btcreate = document.createElement("button")
     btcreate.textContent = "Apagar"
-    btcreate.classList.add("btdel");
-    document.getElementById("buttonarea").appendChild(btcreate);
+    btcreate.classList.add("btdel")
+    document.getElementById("buttonarea").appendChild(btcreate)
 }
+
 function editButton() {
-    var btcreate = document.createElement("button");
+    var btcreate = document.createElement("button")
     btcreate.textContent = "Editar"
-    btcreate.classList.add("btedit");
-    document.getElementById("buttonarea").appendChild(btcreate);
+    btcreate.classList.add("btedit")
+    document.getElementById("buttonarea").appendChild(btcreate)
 }
 
 function appendText() {
-    
+
 }
 
 function editNote() {
@@ -39,5 +37,33 @@ function deleteNote() {
 }
 
 function darkMode() {
-   
+    //background color, navbar color
+    var colors = ['#292534', '#242338']
+    document.body.style.background = colors[0]
+    document.querySelector('.top').style.background = colors[1]
+    document.querySelector('.darkMode').textContent = "Default Color"
 }
+
+function defaultColor() {
+    //background color, navbar color
+    var colors = ['#fff1ff', '#9A93F2']
+    document.body.style.background = colors[0];
+    document.querySelector('.top').style.background = colors[1]
+    document.querySelector('.darkMode').textContent = "Dark Mode"    
+}
+
+document.querySelector('.save').addEventListener("click", function() {
+    createNote()
+    divtoappendButtons()
+    delButton()
+    editButton()
+})
+
+document.querySelector('.darkMode').addEventListener("click", function() {
+    var check = "Dark Mode"
+    if (document.querySelector('.darkMode').textContent == check) {
+        darkMode()
+    } else {
+        defaultColor()
+    }
+})
